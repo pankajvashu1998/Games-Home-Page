d6219626c695851b3fac26f08b58c9954cfee8c4
Pankaj Kumar <pk85764798@gmail.com>
2025-09-22 15:30:49 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import App from "./App.jsx";
 import AboutMe from "./AboutMe.jsx";
 import ServicesPage from "./Services.jsx";
 import ContactPage from "./Contact.jsx";
-import ChessGame from "./chess/components/ChessBoard.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -22,7 +22,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App/>}>
           <Route path="/" element={<Home/>} />
           <Route path="/tic-tac-toe" element={<TicTacToeGame/>} />
-          <Route path="/chess" element={<ChessGame/>} />
+          
         </Route>
 
         <Route path="/about" element = {<AboutMe/>}/>
